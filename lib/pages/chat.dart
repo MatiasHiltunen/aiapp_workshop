f91d42c6d9bbcecc4b982f93e7ed3ca7d1eb5559
Matias Hiltunen <matias.s.j@hotmail.com>
2024-05-04 16:54:35 +0300

    fix the textfield layout of chat

--- lib/pages/chat.dart
+++ lib/pages/chat.dart
@@ -1,29 +1,38 @@
 import 'package:flutter/material.dart';
 
 class ChatPage extends StatelessWidget {
+  const ChatPage({super.key});
+
   @override
   Widget build(BuildContext context) {
-    return Column(
-      children: [
-        Text("test"),
-        /* Container(
-          child: ListView(
-            children: [],
-          ),
+    return LayoutBuilder(builder: (context, constraints) {
+      return SizedBox(
+        height: constraints.maxHeight,
+        width: constraints.maxWidth,
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.spaceBetween,
+          children: [
+            const Text("test"),
+            const Spacer(),
+            Row(
+              mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+              children: [
+                SizedBox(
+                    width: constraints.maxWidth * 0.8,
+                    child: const TextField(
+                      decoration: InputDecoration(border: OutlineInputBorder()),
+                    )),
+                IconButton.outlined(
+                    onPressed: () {}, icon: const Icon(Icons.arrow_forward)),
+              ],
+            ),
+            SizedBox(
+              height: 100,
+              width: constraints.maxWidth,
+            )
+          ],
         ),
-        SizedBox(
-          height: 100,
-          width: 200,
-          child: TextField(
-            onChanged: (value) {
-              print(value);
-            },
-          ),
-        ),
-        IconButton.outlined(onPressed: (){
-        
-        }, icon: Icon(Icons.arrow_forward)) */
-      ],
-    );
+      );
+    });
   }
 }
