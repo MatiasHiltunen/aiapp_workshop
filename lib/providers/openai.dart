da1fdd8d44f9265fe8de4e32ffb657b27089b6e1
Matias Hiltunen <matias.s.j@hotmail.com>
2024-05-04 11:52:09 +0300

    first commit

---
+++ lib/providers/openai.dart
@@ -0,0 +1,14 @@
+
+import 'package:flutter/material.dart';
+
+class OpenAIProvider extends ChangeNotifier {
+
+  List<String> messages = [];
+
+  void createMessage(String message){
+
+    messages.add(message);
+
+    notifyListeners();
+  }
+}
